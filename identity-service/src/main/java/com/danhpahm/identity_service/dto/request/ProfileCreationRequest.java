7752de24da2b1c3df4a1db8259cb9dadc0d294e3
Danh100121 <danh100121@gmail.com>
2026-01-23 00:23:52 +0700

    add-openfeign-clinet: add openfeign client

--- identity-service/src/main/java/com/danhpahm/identity_service/dto/request/ProfileCreationRequest.java
+++ identity-service/src/main/java/com/danhpahm/identity_service/dto/request/ProfileCreationRequest.java
@@ -1,18 +1,19 @@
-package com.danhpahm.identity_service.dto.response;
+package com.danhpahm.identity_service.dto.request;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
-import java.util.Set;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class UserResponse {
-    String id;
-    String username;
-    Set<RoleResponse> roles;
+public class ProfileCreationRequest {
+    String userId;
+    String firstName;
+    String lastName;
+    LocalDate dob;
+    String city;
 }
